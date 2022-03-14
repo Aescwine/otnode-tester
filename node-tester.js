df5635da79ae3aaca41aee59e0ba1a37b32e2616
root <root@DESKTOP-VQFU3HR.localdomain>
2022-03-14 14:11:16 +0000

    Update tester

--- node-tester.js
+++ node-tester.js
@@ -3,13 +3,14 @@ const randomstring = require("randomstring");
 const Logger = require("./logger");
 const FormData = require("form-data");
 const fs = require("fs");
-
+const SparqlParser = require("sparqljs").Parser;
 class NodeTester {
 
     constructor(options) {
         this.logger = new Logger("debug");
         this.nodeBaseUrl = 'http://0.0.0.0:8900';
         this.defaultTimeoutInSeconds = 30;
+        this.sparqlParser = new SparqlParser({ skipValidation: false });
     }
 
     async run() {
@@ -39,37 +40,56 @@ class NodeTester {
 
                 await this.sleepForMilliseconds(15 * 1000); // sleep for 15 seconds
 
-                let publishedUal = await this.getProvisionResult(publishHandlerId).then(provisionResult => {
+                let publishedData = await this.getProvisionResult(publishHandlerId).then(provisionResult => {
                     this.logger.debug(`Provision result received: ${provisionResult.data}`);
 
-                    let ual = provisionResult.data.data.metadata.UALs[0];
-                    return ual;
+                    return provisionResult.data;
                 });
 
-                // this.logger.debug(`Resolve UAL ${publishedUal}`);
-                // for (let i = 0; i < 1000; i++) {
-                //     this.resolveRequest(publishedUal);
-                //     await this.sleepForMilliseconds(500); // sleep for half a second
-                // }
+                let assertionId = publishedData.data.id;
+                let ual = publishedData.data.metadata.UALs[0];
+
+                this.logger.debug(`Resolve UAL ${ual}`);
+                for (let i = 0; i < 500; i++) {
+                    this.resolveRequest(ual, i + 1);
+                    await this.sleepForMilliseconds(750); // sleep for half a second
+                }
 
                 this.logger.debug(`Entity search for keyword ${keyword}`);
                 for (let i = 0; i < 100; i++) {
-                    this.searchRequest({query: keyword, resultType: "entities"});
+                    this.searchRequest({ query: keyword, resultType: "entities" }, i + 1);
                     await this.sleepForMilliseconds(1000); // sleep for 1 second
                 }
 
                 this.logger.debug(`Assertion search for keyword ${keyword}`);
                 for (let i = 0; i < 100; i++) {
-                    this.searchRequest({query: keyword, resultType: "assertions"});
+                    this.searchRequest({ query: keyword, resultType: "assertions" }, i + 1);
                     await this.sleepForMilliseconds(1000); // sleep for 1 second
                 }
 
                 this.logger.debug(`Sparql query for keyword ${keyword}`);
+
+                let sparqlQuery = 'PREFIX schema: <http://schema.org/> ' +
+                                  'CONSTRUCT { ?s schema:hasKeywords "' + keyword + '". } ' +
+                                  'WHERE { ' +
+                                    'GRAPH ?g { ' +
+                                      '?s schema:hasKeywords "' + keyword + '" . ' +
+                                    '}' +
+                                  '}';
+
                 for (let i = 0; i < 1000; i++) {
-                    this.resolveRequest(publishedUal);
-                    await this.sleepForMilliseconds(500); // sleep for half a second
+                    this.queryRequest({ query: sparqlQuery }, i + 1);
+                    await this.sleepForMilliseconds(750); // sleep for half a second
                 }
 
+                let proofQuery = `["<did:dkg:${assertionId}> <http://schema.org/hasKeywords> \"${keyword}\" ."]`;
+
+                for (let i = 0; i < 50; i++) {
+                    this.proofsRequest({ nquads: proofQuery }, i + 1);
+                    await this.sleepForMilliseconds(10 * 1000); // sleep for 10 seconds
+                }
+
+                await this.sleepForMilliseconds(60 * 1000); // sleep for 1 minute
 
             } catch (e) {
                 this.logger.error(e);
@@ -129,8 +149,8 @@ class NodeTester {
         return axios(axios_config);
     }
 
-    resolveRequest(ual) {
-        this.logger.debug(`Sending resolve request. ${new Date().toGMTString()}`);
+    resolveRequest(ual, requestNumber = 1) {
+        this.logger.debug(`Sending resolve request number ${requestNumber}. ${new Date().toGMTString()}`);
         const form = new FormData();
         let ids = `ids=${ual}`;
 
@@ -145,8 +165,8 @@ class NodeTester {
         return axios(axios_config);
     }
 
-    searchRequest(options) {
-        this.logger.debug(`Sending search request. ${new Date().toGMTString()}`);
+    searchRequest(options, requestNumber = 1) {
+        this.logger.debug(`Sending search request number ${requestNumber}. ${new Date().toGMTString()}`);
         const form = new FormData();
         let query = options.query;
         let resultType = options.resultType;
@@ -162,8 +182,8 @@ class NodeTester {
         return axios(axios_config);
     }
 
-    queryRequest(options) {
-        this.logger.debug(`Sending query request. ${new Date().toGMTString()}`);
+    queryRequest(options, requestNumber = 1) {
+        this.logger.debug(`Sending query request number ${requestNumber}. ${new Date().toGMTString()}`);
         const form = new FormData();
         let type = options.type ? options.type : "construct";
         let sparqlQuery = options.query;
@@ -184,8 +204,8 @@ class NodeTester {
         return axios(axios_config);
     }
 
-    getProofsRequest(options) {
-        this.logger.debug(`Sending get proofs request. ${new Date().toGMTString()}`);
+    proofsRequest(options, requestNumber = 1) {
+        this.logger.debug(`Sending get proofs request number ${requestNumber}. ${new Date().toGMTString()}`);
         const form = new FormData();
         let nquads = options.nquads;
         form.append("nquads", JSON.stringify(nquads));
